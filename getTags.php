bf2b94f51d8c88a44b8ef942dc94287c8a680ecb
wadleedooman <franciscozampieri1218@gmail.com>
2014-10-07 18:26:21 -0400

    Added SQL query

--- getTags.php
+++ getTags.php
@@ -1,17 +1,19 @@
 <?php
 
-$connection=mysqli_connect('localhost','root','csip','');
+$connection=mysqli_connect('localhost','root','csip','its');
 
 if (mysqli_connect_errno()) {
   echo "failed to connect " . mysqli_connect_error();
 }
 
-$sql = "SELECT id, name from its.tags;";
+$sql = "SELECT id, name from tags;";
+$sql1 = "SELECT * FROM intelligent_review_Spring 2014 AS it left outer join tags AS c ON c.id = it.concept left outer join questions AS q ON q.id = it.question LIMIT 5;"
+echo $sql;
 $arr = mysqli_query($connection, $sql);
 
 $filename = "/root/repos/Its_Spring_2014/tagsData.json";
 $file = fopen($filename, "w");
-
+/*
 fwrite($file, "\"tags\":[
 	");
 
@@ -24,5 +26,5 @@ while ($row = mysqli_fetch_array($arr)) {
 fwrite($file, "]");
 
 fclose($file);
-
+*/
 ?>
