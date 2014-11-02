65051910af4e1eb4a18c30a6be4be773223402d6
senlin13gt <senlin13gt@gmail.com>
2014-11-02 15:56:35 -0800

    add update position and correct some issues for nextQ, preQ, mouseover

--- script.js
+++ script.js
@@ -43,20 +43,20 @@ function setNodeData(branch){
 var nodes = [
       { name: "1st Parent ", ndx: 0, questionID: branch[0]},
       { name: "2nd Parent ", target: [0], questionID: "Next Branch"},
-      { name: "2nd Tier", ndx: 1, target: [0], questionID: branch[1]},
-      { name: "2nd Tier", ndx: 2, target: [0], questionID: branch[2]},
-      { name: "3rd Tier", ndx: 3, target: [2], questionID: branch[3]},
-      { name: "3rd Tier", ndx: 4, target: [2], questionID: branch[4]},
-      { name: "3rd Tier", ndx: 5, target: [3], questionID: branch[5]},
-      { name: "3rd Tier", ndx: 6, target: [3], questionID: branch[6]},
-      { name: "4th Tier", ndx: 7, target: [4], questionID: branch[7]},
-      { name: "4th Tier", ndx: 8, target: [4], questionID: branch[8]},
-      { name: "4th Tier", ndx: 9, target: [5], questionID: branch[9]},
-      { name: "4th Tier", ndx: 10, target: [5], questionID: branch[10]},
-      { name: "4th Tier", ndx: 11, target: [6], questionID: branch[11]},
-      { name: "4th Tier", ndx: 12, target: [6], questionID: branch[12]},
-      { name: "4th Tier", ndx: 13, target: [7], questionID: branch[13]},
-      { name: "4th Tier", ndx: 14, target: [7], questionID: branch[14]}
+      { name: "2nd Tier", ndx: 2, target: [0], questionID: branch[1]},
+      { name: "2nd Tier", ndx: 3, target: [0], questionID: branch[2]},
+      { name: "3rd Tier", ndx: 4, target: [2], questionID: branch[3]},
+      { name: "3rd Tier", ndx: 5, target: [2], questionID: branch[4]},
+      { name: "3rd Tier", ndx: 6, target: [3], questionID: branch[5]},
+      { name: "3rd Tier", ndx: 7, target: [3], questionID: branch[6]},
+      { name: "4th Tier", ndx: 8, target: [4], questionID: branch[7]},
+      { name: "4th Tier", ndx: 9, target: [4], questionID: branch[8]},
+      { name: "4th Tier", ndx: 10, target: [5], questionID: branch[9]},
+      { name: "4th Tier", ndx: 11, target: [5], questionID: branch[10]},
+      { name: "4th Tier", ndx: 12, target: [6], questionID: branch[11]},
+      { name: "4th Tier", ndx: 13, target: [6], questionID: branch[12]},
+      { name: "4th Tier", ndx: 14, target: [7], questionID: branch[13]},
+      { name: "4th Tier", ndx: 15, target: [7], questionID: branch[14]}
 ];
   return nodes;
 }
@@ -74,6 +74,7 @@ nextB.on('click', function(){
     question.property("value", json.question[qID].question);
     tooltip.html(json.question[qID].concept_name);
     question.html(json.question[qID].question);
+    updatePosition();
   }
 });
 
@@ -87,7 +88,6 @@ for (var i = 0; i< nodes.length; i++) {
                         target: nodes[nodes[i].target[x]]
                   })
             }
-      //debugger;
       }
 }
 
@@ -109,7 +109,6 @@ var node = myTree.selectAll('circle')
   .data(nodes).enter()
   .append('g');
 
-  console.log(node);
 node.append('circle')
   .attr('cx', function(d, i) { 
         return d.x;
@@ -117,19 +116,38 @@ node.append('circle')
   .attr('cy', function(d, i) {
         return d.y;
         })
-  .attr('r', circleWidth )
+  .attr('r', function(d) {
+    if (currentQuestion == d.ndx) return circleWidth*2;
+    return circleWidth;
+    })
   .attr('fill', function(d, i) {
         if (i <= 1) { return palette.blue}
         else if (i == 2 || i == 3) {return palette.green}
         else if (i >= 4 && i < 8) { return palette.red}
         else { return palette.yellow}
   })
+  .style('opacity', function(d){
+    if (d.ndx == currentQuestion) return .5;
+    return 1;
+  })
 
+function updatePosition(){
+  node.select("circle")
+  .attr('r', function(d){
+    if (d.ndx == currentQuestion) return circleWidth*2;
+    return circleWidth;
+  })
+  .style('opacity', function(d){
+    if (d.ndx == currentQuestion) return .5;
+    return 1;
+  })
 
+}
 
 var tempColor;
 //console.log(nodes);
 force.on('tick', function(e) {
+
   node
   .attr('transform', function(d, i) {
         d.fixed = true;
@@ -179,13 +197,9 @@ force.on('tick', function(e) {
   .on('mouseover', function(d){
     //debugger;
     //d.circle.r = circleWidth * 2
-    d3.select(this).select("circle").attr('r', circleWidth*3 )
     tooltip.transition()
             .style('opacity', 0.9)
     tempColor = this.style.fill;
-    d3.select(this)
-            .style('opacity', .5)
-            .style('fill', palette.green)
         //console.log(d.questionID);
     if (document.getElementById("manual").checked == true) {
       tooltip.html(json.question[d.questionID].concept_name);
@@ -195,26 +209,16 @@ force.on('tick', function(e) {
       question.property("value", json.question[d.questionID].answers);
       // start Francisco's code
       currentQuestion = d.ndx;
-      if (currentQuestion == 1){
-        currentAnswer = json.question[nodes[2].questionID].answers;
-      } else {
-        currentAnswer = json.question[nodes[currentQuestion].questionID].answers;
-      }
+      updatePosition();
+      currentAnswer = json.question[nodes[currentQuestion].questionID].answers;
       console.log(currentAnswer);
       if (currentQuestion == 0) {
         tooltip.html(json.question[nodes[0].questionID].concept_name);
         question.html(json.question[nodes[0].questionID].question);
         question.property("value", json.question[nodes[0].questionID].question);
       }
-      currentQuestion++;
     }
   })
-  .on('mouseout', function(d) {
-    d3.select(this).select("circle").attr('r', circleWidth )
-        d3.select(this)
-            .style('opacity', 1)
-            .style('fill', tempColor)
-    })
   link
     .attr('x1', function(d) { return d.source.x })
     .attr('y1', function(d) { return d.source.y })
@@ -235,78 +239,82 @@ var nextQ = d3.select("#next-question");
 var qID = nodes[0].questionID;
 
 function nextQFunc() {
-  if (currentQuestion != 0) {
-    currentQuestion++;
-  }
+  currentQuestion++;
+  if (currentQuestion == 1) currentQuestion++;
   if (currentQuestion > 15) {
     currentQuestion = 0;
     currentBranch++;
     nodes = setNodeData(json.branch[currentBranch]); 
     node.data(nodes);
   }
-  if (currentQuestion == 1) {
-    qID = nodes[0].questionID;
-    currentQuestion++;
-  } else {
-    qID = nodes[currentQuestion].questionID;
-  }
-  //qID = nodes[currentQuestion].questionID;
+  currentAnswer = json.question[nodes[currentQuestion].questionID].answers;
+ // console.log("next question " + currentAnswer);
+  qID = nodes[currentQuestion].questionID;
+  answer.html(json.question[qID].answers);
   question.html(json.question[qID].question);
   tooltip.html(json.question[qID].concept_name);
-  if (currentQuestion == 0) {
-    currentQuestion = 1;
-  }
 }
 
 nextQ.on('click', function() {
   if (document.getElementById("manual").checked == true) {
-    nextQFunc();    
+    nextQFunc();
+    updatePosition();
   }
 });
 
 var prevQ = d3.select("#pre-question");
-
 function prevQFunc() {
   currentQuestion--;
-  if (currentQuestion < 1) {
-    currentQuestion = 1;
+  //on start
+  if (currentBranch == 0 && currentQuestion == -1){
+    currentQuestion++;
   }
+  //avoid the dump question 
   if (currentQuestion == 1) {
-    qID = nodes[0].questionID;
-    question.html(json.question[qID].question);
-    tooltip.html(json.question[qID].concept_name);
-  } else {
-    qID = nodes[currentQuestion].questionID;
-    question.html(json.question[qID].question);
-    tooltip.html(json.question[qID].concept_name);
+    currentQuestion = 0;
   }
+  //from previous branch
+  if (currentQuestion < 0 && currentBranch > 0) {
+    currentQuestion = 15;
+    currentBranch--;
+    nodes = setNodeData(json.branch[currentBranch]); 
+    node.data(nodes);
+  } 
+   currentAnswer = json.question[nodes[currentQuestion].questionID].answers;
+   qID = nodes[currentQuestion].questionID;
+   answer.html(json.question[qID].answers);
+   question.html(json.question[qID].question);
+   tooltip.html(json.question[qID].concept_name);
 }
 
 prevQ.on('click', function() {
   if (document.getElementById("manual").checked == true) {
     prevQFunc();
+    updatePosition();
   }
 });
 
 var submitB = d3.select("#submit");
 submitB.on('click', function() {
-  if (currentQuestion == 1) {
-    currentAnswer = json.question[nodes[2].questionID].answers;
-  } else {
+     //console.log("current question is from submit" + currentQuestion);
     currentAnswer = json.question[nodes[currentQuestion].questionID].answers;    
-  }
+
   console.log(currentAnswer);
   if (currentAnswer == currentAnswer*wrong){
     //records.append("right");
-    records += "right";
+    records += " right";
   } else {
     //records.append("wrong");
-    records += "wrong";
+    records += " wrong";
   }
   wrong = wrong * -1;
   nextQFunc();
+  updatePosition();
+   //console.log("current question is from after" + currentQuestion);
   console.log(records);
 });
+
+
 
 force.start();
 })
