4fcf540730de3524219bbeb63b0ac3f160079fea
f-zampieri <franciscozampieri1218@gmail.com>
2015-04-02 01:32:21 -0400

    Added tabs to further divide the left portion of the webpage into TREE, STATISTICS, and SETTINGS; added TimerCircles

---
+++ tabScript.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function() {
+    // TimerCircles!
+	$("#stats-area").TimeCircles(); 
+	console.log("READY FOR CLICKING");
+    jQuery('.tabs-area .tab-links a').on('click', function(e)  {
+    	console.log("CLICKED");
+        var currentAttrValue = jQuery(this).attr('href');
+ 		console.log(currentAttrValue);
+        // Show/Hide Tabs
+        jQuery('.tabs-area ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active-tab').siblings().removeClass('active-tab');
+
+
+        e.preventDefault();
+    });
+});
